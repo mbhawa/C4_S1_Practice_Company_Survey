c11abe0f959fcd5c9fb26490db84b9f118572ea2
Mridul <mridulbhawa@gmail.com>
2022-09-01 12:48:09 +0530

    final commit

--- src/main/java/com/jap/AverageAge.java
+++ src/main/java/com/jap/AverageAge.java
@@ -2,26 +2,23 @@ package com.jap;
 
 public class AverageAge {
 
-    public String averageAgeCalculator(String[] ageRawData)
-    {
+    public String averageAgeCalculator(String[] ageRawData) {
         float averageAge;
-        int numberOfEmployees=ageRawData.length;
+        int numberOfEmployees = ageRawData.length;
 
-        try{ //Write the logic
+        try { //Write the logic
 
-            float totalAge=0;
-            for(String index: ageRawData){
-            totalAge=totalAge+Float.parseFloat(index);
+            float totalAge = 0;
+            for (String index : ageRawData) {
+                totalAge = totalAge + Float.parseFloat(index);
             }
-            averageAge=totalAge/numberOfEmployees;
+            averageAge = totalAge / numberOfEmployees;
             return Float.toString(averageAge);
             //Handle specific exception
-        }catch (NumberFormatException exception)
-        {
+        } catch (NumberFormatException exception) {
             return exception.toString();
         }
     }
-
 
 
 }
